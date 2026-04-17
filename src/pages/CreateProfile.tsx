78c975a63ee6229fd4c6b296d5bd5fd93f1dfa31
romchi-service-apps <romchi-service-apps@users.noreply.github.com>
2026-04-17 11:31:53 +0500

    line removed

--- src/pages/CreateProfile.tsx
+++ src/pages/CreateProfile.tsx
@@ -6,7 +6,6 @@ import { SpecIcon } from '../SpecIcon';
 import { api, auth } from '../api';
 import { MapPicker } from '../components/MapPicker';
 
-
 const SPECS = ['Termo', 'PVX', 'Alyumin'];
 const RED = '#DC2626';
 const RED_50 = '#FEE2E2';
